a46ba0c60a4abe05744cbc7f2161c9aecd45934c
Fatih <fatihmertdogancan@hotmail.com>
2020-12-13 20:55:50 +0300

    supercharged lib add, color class relaxed

--- lib/app/colors.dart
+++ lib/app/colors.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:supercharged/supercharged.dart';
 
 class ColorTable {
-  static final backgroundColor = fromHex('F4F7FC');
+  static final backgroundColor = 'FF0000'.toColor();
+  static final backgroundColor2 = 'yellow'.toColor();
   static final white90 = Color.fromRGBO(255, 255, 255, 0.90);
-
-  // Ex use: ColorTable.fromHex('#ff0000') | ColorTable.fromHex('ff0000')
-  static Color fromHex(String hexColor) {
-    final hexCode = hexColor.replaceAll('#', '');
-    return Color(int.parse('FF$hexCode', radix: 16));
-  }
-
 }
